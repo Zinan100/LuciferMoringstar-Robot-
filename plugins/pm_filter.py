2549ab4494d3afd9cfaf5fd03edd00b229d1e3da
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-20 17:19:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1396,15 +1396,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         free = 536870912 - monsize
         monsize = get_size(monsize)
         free = get_size(free)
-        await query.message.edit_text(
-            text="▣▢▢"
-        )
-        await query.message.edit(
-            text="▣▣▢"
-        )
-        await query.message.edit(
-            text="▣▣▣"
-        )
         await query.message.edit(
             text=script.STATUS_TXT.format(total, users, chats, monsize, free),
             reply_markup=reply_markup,
